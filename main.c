49a6ec354897e1e24bf5baad2f88b1d826914e8d
Vadim Vesnin <9papazoglo@gmail.com>
2017-05-03 20:39:29 +0700

    Implement data input

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
-	printf("Hello World");
+	setlocale(LC_ALL,"RUS");
+	printf("Hello World\n");
+	int m,t;
+	printf("¬ведите вносимую сумму (>=10000 рублей): ");
+	scanf("%d",&m);
+	printf("¬ведите срок вклада (<=365 дней): ");
+	scanf("%d",&t);
 	return 0;
 }
