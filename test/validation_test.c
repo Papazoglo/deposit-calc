11f3a426a161438d9123e71fb96163941801b173
Vadim Vesnin <9papazoglo@gmail.com>
2017-05-29 04:40:19 +0700

    Added validation tests

---
+++ test/validation_test.c
@@ -0,0 +1,24 @@
+#include "ctest.h"
+#include "deposit.h"
+
+CTEST(validation_money, all_cases)
+{
+	int res = money_check(9000);
+	int exp = 1;
+	ASSERT_EQUAL(exp, res);
+
+	res = money_check(23000);
+	exp = 0;
+	ASSERT_EQUAL(exp, res);
+}
+
+CTEST(validation_term, all_cases)
+{
+	int res = term_check(400);
+	int exp = 1;
+	ASSERT_EQUAL(exp, res);
+	
+	res = term_check(232);
+	exp = 0;
+	ASSERT_EQUAL(exp, res);
+}
